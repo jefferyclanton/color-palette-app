bbc37fe51d7587badae32458237bbaf6ebb822cb
jefferyclanton <jefferyaclanton@gmail.com>
2019-03-29 18:22:23 -0400

    Add color picker

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-  import React from 'react';
-  import './App.css';
-  import chroma from 'chroma-js';
+import React from 'react';
+import { PhotoshopPicker } from 'react-color';
+import chroma from 'chroma-js';
 
-
-  class App extends React.Component {
-    constructor(props) {
-    super(props);
-    this.state = {
-      hue: '',
-      color: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    }
-    handleChange(e) {
-      this.setState({
-        hue: e.target.value,
-        color: chroma(e.target.value, 1, 0.5, "hsl")
-      })
-    }
-
-    render() {
-      const style = {
-        background: this.state.color,
-        height: 200
+class App extends React.Component {
+ state = {
+    background: {
+        hsl: {
+          h: 0,
+          s: 0,
+          l: .20,
+          a: 1
+        }
       }
-      return (
-        <div style={style}>
-        <h2 style={{marginTop:0}}>Enter Hue In Degrees</h2>
-          <input value={this.state.hue} onChange={this.handleChange}></input>
-        </div>
-      );
-    }
+  };
+
+  handleChange = (color) => {
+    this.setState({
+      background: color.hsl
+    })
   }
+
+  render() {
+    const style = {
+      height: 250,
+      width: 250,
+      backgroundColor: chroma(this.state.background.h, this.state.background.s, this.state.background.l, "hsl")
+    }
+    return (
+      <div>
+        <div>
+    <PhotoshopPicker header="Choose Your Base Color" color={this.state.background} onChange={this.handleChange} />;
+    </div>
+    <div style={style}></div>
+    </div>
+    )
+  }
+}
 
   export default App;
