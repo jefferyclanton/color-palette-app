b689840990016095516cba5e38c2ec4119a14906
jefferyclanton <jefferyaclanton@gmail.com>
2019-04-16 12:03:12 -0400

    Change color picker and add some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PhotoshopPicker } from 'react-color';
+import { SketchPicker } from 'react-color';
 import SimpleSelect from './drop-down-menu';
 import chroma from 'chroma-js';
 import ColorAmount from './color-amount';
@@ -17,8 +17,8 @@ class App extends React.Component {
       background: {
           hsl: {
             h: 0,
-            s: 1,
-            l: 0.5,
+            s: 0,
+            l: 1.0,
             a: 1
           }
         },
@@ -30,12 +30,17 @@ class App extends React.Component {
     this.harmonyChange = this.harmonyChange.bind(this);
     this.numberOfColors = this.numberOfColors.bind(this);
     this.hueSeparation = this.hueSeparation.bind(this);
+    this.handleAccept = this.handleAccept.bind(this);
   }
 
   handleChange = (color) => {
     this.setState({
-      background: color.hsl
+      background: color.hsl,
     })
+  };
+
+  handleAccept = () => {
+    
   };
 
   harmonyChange = (event) => {
@@ -57,29 +62,29 @@ class App extends React.Component {
   render() {
     const harmony = this.state.harmony;
     const style = {
-      height: 275,
-      width: 275,
+      height: 100,
+      width: '100%',
       backgroundColor: chroma(this.state.background.h, this.state.background.s, this.state.background.l, "hsl"),
-      borderRadius: 500,
-      margin: 'auto',
+      marginTop: '2%',
     }
 
     return (
       <div>
-      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', marginTop: 25}}>
-        <div>
-    <PhotoshopPicker header="Choose Your Base Color" color={this.state.background} onChange={this.handleChange}  />;
-    </div>
-    <div style={style}></div>
-      </div>
+        <h1 style={{textAlign:'center'}}>Choose Your Base Color</h1>
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+      <SketchPicker color={this.state.background} onChange={this.handleChange} width={350} disableAlpha={true} />
       <SimpleSelect styling={this.state.background} onChange={this.harmonyChange} harmonyChange={harmony} />
       <ColorAmount value={this.state.harmony} amount={this.state.numberOfColors} style={this.state.background} colors={this.numberOfColors}/>
-      <HueSeparation hueSeparation={this.state.hueSeparation} setSeparation={this.hueSeparation} selection={this.state.harmony} onSubmit={this.handleSubmit} />
+      <HueSeparation hueSeparation={this.state.hueSeparation} setSeparation={this.hueSeparation} selection={this.state.harmony} onSubmit={this.handleSubmit} />    
+      </div>
+      <div style={style}></div>
+      <div>
       <Monochromatic numberOfColors={this.state.numberOfColors} choice={this.state.harmony} colors={this.state.background} />
       <Complementary colors={this.state.background} choice={this.state.harmony} />
       <Triadic choice={this.state.harmony} colors={this.state.background} />
       <Split choice={this.state.harmony} colors={this.state.background} separation={this.state.hueSeparation} />
       <Analogous choice={this.state.harmony} colors={this.state.background} separation={this.state.hueSeparation} />
+      </div>
     </div>
     )
   }
